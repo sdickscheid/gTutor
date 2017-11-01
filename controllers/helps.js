3a4798258f961324210b604a432caebfd5d9f8bc
bdoring <doringbarbara@gmail.com>
2017-11-01 15:56:25 -0700

    message system ready

---
+++ controllers/helps.js
@@ -0,0 +1,92 @@
+const knex = require("../db/knex.js");
+
+module.exports = {
+  index: function(req, res) {
+      knex('posts')
+        .select('posts.id', 'posts.topic', 'posts.content', 'posts.title', 'users.first_name', 'users.last_name')
+        .join('users', 'users.id', 'posts.user_id')
+        .where('posts.id', req.params.postID)
+        .then((foundPost) => {
+            knex('users')
+              .where('users.id', req.session.user)
+              .then((loggedUser) => {
+                res.render('b_offerHelp', {post: foundPost[0], user: loggedUser[0]});
+              });
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  },
+
+    createOne: function(req, res) {
+      console.log("req.body", req.body);
+      console.log("req.params", req.params.postID);
+      let newHelp = {
+        post_id: Number(req.params.postID),
+        user_id: Number(req.session.user)
+      }
+      knex('posts')
+        .update({status: 'PENDING'})
+        .where('posts.id', req.params.postID)
+        .then((result) => {
+          console.log("result", result);
+          knex('helpers')
+            .insert(newHelp, '*')
+            .then((newEntry) => {
+              console.log("ERROR");
+              let newMessage = {
+                helper_id: newEntry[0].id,
+                user_id: Number(req.session.user),
+                message: req.body.message
+              }
+              knex('messages')
+                .insert(newMessage, '*')
+                .then((addedMessage) => {
+                  console.log(addedMessage);
+                  res.redirect(`/gTutor/posts`);
+                })
+            })
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    },
+    action: function(req, res) {
+      if (req.params.action === "accept") {
+        knex('helpers')
+          .where('id', req.params.helpID)
+          .then((help) => {
+            knex('helpers')
+              .update({status: "ACCEPTED"})
+              .where('id', req.params.helpID)
+              .then(() => {
+                knex('helpers')
+                .update({status: "REJECTED"})
+                .whereNot('id', req.params.helpID)
+                .then(() => {
+                  knex('posts')
+                    .update({status: "CLOSED"})
+                    .where('id', help[0].post_id)
+                    .then(() => {
+                      res.redirect('/gTutor/posts');
+                    })
+                })
+              })
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+
+      } else {
+        knex('helpers')
+          .update({status: "REJECTED"})
+          .where('id', req.params.helpID)
+          .then(() => {
+            res.redirect('/gTutor/posts');
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      }
+    }
+}
--- controllers/posts.js
+++ controllers/posts.js
@@ -2,43 +2,110 @@ const knex = require("../db/knex.js");
 
 module.exports = {
   getAll: function(req, res) {
-    knex('posts')
-      .join('users', 'users.id', 'posts.user_id')
-      .orderBy('posts.id', 'DESC')
-      .whereNot('posts.status', 'CLOSED')
-      .then((postsList) => {
-        knex('helpers')
-          .then((allHelps) => {
-            let user = req.session.user;
+      knex('users')
+        .where('users.id', req.session.user)
+        .then((user) => {
+          knex('posts')
+            .select('posts.id AS posts.id', 'user_id', 'posts.topic', 'posts.title', 'posts.content', 'posts.status', 'posts.created_at', 'users.first_name', 'users.last_name', 'users.email', 'users.campus', 'users.cohort', 'users.type')
+            .join('users', 'users.id', 'posts.user_id')
+            .orderBy('posts.id', 'DESC')
+            // .whereNot('posts.status', 'CLOSED')
+            .then((postsList) => {
+              knex('helpers')
+                .then((allHelps) => {
+                  knex('messages')
+                    .select('messages.id', 'messages.helper_id', 'messages.user_id', 'messages.message', 'users.first_name', 'users.last_name', 'users.cohort', 'users.type', 'helpers.post_id', 'helpers.status')
+                    .join('users', 'users.id', 'messages.user_id')
+                    .join('helpers', 'helpers.id', 'messages.helper_id')
+                    .then((allMessages) => {
+                      let messagesReceived = [];
 
-            let userPosts = postsList.filter(posts => posts.id == req.params.id);
-            res.render('b_newPostForm', {posts: postsList, userName: user, personalPosts: userPosts, helps: allHelps, loggedUserID: req.params.id});
-          })
+                      let userPosts = postsList.filter(posts => posts.user_id == req.session.user);
 
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+
+                      let helpsOffered = userPosts.filter(posts => posts.status === "PENDING");
+
+                      // console.log('helpsOffered', helpsOffered);
+
+                      if (helpsOffered[0]) {
+                        let helpID = allHelps.filter((helpers) => helpers.post_id === helpsOffered[0]["posts.id"])
+
+                        // console.log('helpID', helpID);
+
+                        // messagesReceived = allMessages.filter((messages, index) => {
+                        //
+                        //   if (index < helpID.length) {
+                        //     if (messages.helper_id === helpID[index].id) {
+                        //       return messages
+                        //     }
+                        //   }
+                        // });
+
+                          for (var i = 0; i < helpID.length; i++) {
+                            let thisArr = allMessages.filter(messages => messages.helper_id === helpID[i].id);
+                            messagesReceived = messagesReceived.concat(thisArr);
+                            console.log("thisArr", thisArr)
+                          }
+
+                        }
+                        // });
+
+                        console.log('messagesReceived', messagesReceived);
+                      //}
+
+                      let messagesSent = [];
+                      let postsOfferedHelp = [];
+
+                      let messagesSentHelpID = allHelps.filter(helpers => helpers.user_id === req.session.user);
+
+                      if (messagesSentHelpID[0]) {
+                        messagesSent = allMessages.filter(messages => messages.helper_id === messagesSentHelpID[0].id);
+
+                        postsOfferedHelp = postsList.filter(posts => posts["posts.id"] === messagesSentHelpID[0].post_id);
+
+                        console.log("postsOfferedHelp", postsOfferedHelp)
+
+                      }
+
+                      console.log("messagesSentHelpID", messagesSentHelpID);
+
+                      console.log('messagesSent', messagesSent);
+
+                      res.render('b_newPostForm', {posts: postsList, userName: user[0].first_name, personalPosts: userPosts, helps: allHelps, loggedUserID: req.session.user, messages: messagesReceived, offeredHelp: postsOfferedHelp, sentMessages: messagesSent});
+                    })
+                })
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
   },
 
   createOne: function(req, res) {
-    let newPost = {
-      user_id: req.params.id,
-      topic: req.body.topic,
-
-      title: req.body.title,
-      content: req.body.content
-
-    }
 
     knex('posts')
-      .insert(newPost, '*')
-      .then((post) => {
-        console.log(post);
-        res.redirect(`/gTutor/${req.params.id}`)
-      })
-      .catch((err) => {
-        console.log(err);
+      .then((allPosts) => {
+        let userHasPost = allPosts.filter(post => post.user_id == req.session.user);
+
+        if (userHasPost[0]) {
+          res.send('Sorry, you can only have one post at a time')
+        } else {
+            let newPost = {
+              user_id: Number(req.session.user),
+              topic: req.body.topic,
+              title: req.body.title,
+              content: req.body.content
+            }
+            knex('posts')
+              .insert(newPost, '*')
+              .then((post) => {
+                console.log(post);
+                res.redirect(`/gTutor/posts`)
+              })
+              .catch((err) => {
+                console.log(err);
+              })
+          }
       })
   }
 }
