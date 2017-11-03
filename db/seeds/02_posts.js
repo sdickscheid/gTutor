018907b807129c2120c19383568a36530b03e0d0
sdickscheid <scott.dickscheid@yahoo.com>
2017-11-03 12:59:19 -0700

    final commit

--- db/seeds/02_posts.js
+++ db/seeds/02_posts.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('posts').insert([
-        {user_id: 1, topic: "JavaScript", title: "Help with Accumulator Patterns", content: "Struggling with understanding and implementing accumulator patterns and iterating over arrays. I am available to meet this Friday (11/3) at 5pm. Please message me if you are available to help. Thanks!", day1: "10 November, 2017", day2: "12 November, 2017", time_start: "10:30AM", time_end: "03:00PM"}
+        {user_id: 1, topic: "JavaScript", title: "Help with Accumulator Patterns", content: "Struggling with understanding and implementing accumulator patterns and iterating over arrays. Please message me if you are available to help. Thanks!", day1: "10 November, 2017", day2: "12 November, 2017", time_start: "10:30AM", time_end: "03:00PM"}
       ]);
     });
 };
